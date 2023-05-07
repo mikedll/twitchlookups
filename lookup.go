992ec891ea7d9de2f827cdf7b11abc9ade1a9964
Michael Rivera <soymrmike@gmail.com>
2023-05-06 21:22:18 -0700

    Refactored to separate files.

--- lookup.go
+++ lookup.go
@@ -13,7 +13,6 @@ import (
 	"net/http"
 	"net/http/httputil"
 	"golang.org/x/oauth2"
-	"golang.org/x/oauth2/clientcredentials"
 	"github.com/joho/godotenv"
 )
 
@@ -27,105 +26,6 @@ type ApiUsersResponse struct {
 	Users  []ApiUser `json:"data"`
 }
 
-func fileExists(filename string) bool {
-    info, err := os.Stat(filename)
-    if os.IsNotExist(err) {
-        return false
-    }
-    return !info.IsDir()
-}
-
-func getClient() *http.Client {
-	oauth2Conf := &clientcredentials.Config{
-		ClientID:     os.Getenv("TWITCH_CLIENT_ID"),
-		ClientSecret: os.Getenv("TWITCH_CLIENT_SECRET"),
-		TokenURL:     "https://id.twitch.tv/oauth2/token",
-	}
-
-	client := oauth2Conf.Client(oauth2.NoContext)
-
-	return client;
-}
-
-func loadToken() *oauth2.Token {
-	tokenFile := ".access_token"
-	
-	if !fileExists(tokenFile) {
-		return nil
-	}
-
-	file, err := os.Open(tokenFile)
-	if err != nil {
-		log.Fatal("Failed to open token file")
-	}
-	defer file.Close()
-
-	var fileBytes []byte;
-	fileBytes, err = io.ReadAll(file)
-	if err != nil {
-		log.Fatal("Got error when reading token file")
-	}
-
-	token := oauth2.Token{}
-	err = json.Unmarshal(fileBytes, &token)
-	if err != nil {
-		log.Fatal("Got error when unmarshalling token bytes")
-	}
-	
-	return &token;
-}
-
-func cacheTokenToDisk(token *oauth2.Token) {
-	if os.Getenv("DEBUG") == "true" {
-		fmt.Printf("Writing token to disk: " + token.AccessToken + "\n")
-	}
-
-	var tokenBytes []byte;
-
-	var err error;
-	tokenBytes, err = json.Marshal(token)
-	if err != nil {
-		log.Fatal("Got error when marshalling token")
-	}
-
-	// fmt.Printf(string(tokenBytes[:]) + "\n")
-
-	os.WriteFile(".access_token", tokenBytes, 0644)
-	if os.Getenv("DEBUG") == "true" {
-		fmt.Printf("Wrote token to disk\n")
-	}
-}
-
-func buildTokenSource() oauth2.TokenSource {
-	token := loadToken()
-	if os.Getenv("DEBUG") == "true" && token != nil {
-		fmt.Printf("Token from file: " + token.AccessToken + "\n")	
-	}
-	
-	oauth2Conf := &clientcredentials.Config{
-		ClientID:     os.Getenv("TWITCH_CLIENT_ID"),
-		ClientSecret: os.Getenv("TWITCH_CLIENT_SECRET"),
-		TokenURL:     "https://id.twitch.tv/oauth2/token",
-	}
-
-	tokenSource := oauth2Conf.TokenSource(oauth2.NoContext)
-	reuseTokenSource := oauth2.ReuseTokenSource(token, tokenSource)
-
-	var err error;	
-	latestToken, err := reuseTokenSource.Token()
-	if err != nil {
-		log.Fatal("Got error when getting token from reuse source")
-	}
-
-	if os.Getenv("DEBUG") == "true" {
-		fmt.Printf("Token from reuse source: " + latestToken.AccessToken + "\n")
-	}
-
-	cacheTokenToDisk(latestToken)
-
-	return reuseTokenSource;
-}
-
 func get(tokenSource oauth2.TokenSource, url string) ([]byte, error) {
 	var err error;
 	var req *http.Request;
@@ -135,7 +35,7 @@ func get(tokenSource oauth2.TokenSource, url string) ([]byte, error) {
 	if err != nil {
 		log.Fatal("Failed to build token from token source")
 	}
-	
+
 	req, err = http.NewRequest("GET", url, nil)
 	if err != nil {
 		log.Fatal("Failed to build request")
@@ -220,11 +120,6 @@ func main() {
 	}
 
 	time.LoadLocation("America/Los_Angeles")
-
 	
-	// body := fmt.Sprintf("Greeting: %s and address of client var is %p\n", os.Getenv("BASIC"), &client)
-	// fmt.Printf(body)
-
-	// client := getClient()
 	getVideos()
 }
---
+++ oauth.go
@@ -0,0 +1,91 @@
+
+package main
+
+import (
+	"os"
+	"fmt"
+	"io"
+	"log"
+	"encoding/json"
+	"golang.org/x/oauth2"
+	"golang.org/x/oauth2/clientcredentials"
+)
+
+func loadToken() *oauth2.Token {
+	tokenFile := ".access_token"
+	
+	if !fileExists(tokenFile) {
+		return nil
+	}
+
+	file, err := os.Open(tokenFile)
+	if err != nil {
+		log.Fatal("Failed to open token file")
+	}
+	defer file.Close()
+
+	var fileBytes []byte;
+	fileBytes, err = io.ReadAll(file)
+	if err != nil {
+		log.Fatal("Got error when reading token file")
+	}
+
+	token := oauth2.Token{}
+	err = json.Unmarshal(fileBytes, &token)
+	if err != nil {
+		log.Fatal("Got error when unmarshalling token bytes")
+	}
+	
+	return &token;
+}
+
+func cacheTokenToDisk(token *oauth2.Token) {
+	if os.Getenv("DEBUG") == "true" {
+		fmt.Printf("Writing token to disk: " + token.AccessToken + "\n")
+	}
+
+	var tokenBytes []byte;
+
+	var err error;
+	tokenBytes, err = json.Marshal(token)
+	if err != nil {
+		log.Fatal("Got error when marshalling token")
+	}
+
+	// fmt.Printf(string(tokenBytes[:]) + "\n")
+
+	os.WriteFile(".access_token", tokenBytes, 0644)
+	if os.Getenv("DEBUG") == "true" {
+		fmt.Printf("Wrote token to disk\n")
+	}
+}
+
+func buildTokenSource() oauth2.TokenSource {
+	token := loadToken()
+	if os.Getenv("DEBUG") == "true" && token != nil {
+		fmt.Printf("Token from file: " + token.AccessToken + "\n")	
+	}
+	
+	oauth2Conf := &clientcredentials.Config{
+		ClientID:     os.Getenv("TWITCH_CLIENT_ID"),
+		ClientSecret: os.Getenv("TWITCH_CLIENT_SECRET"),
+		TokenURL:     "https://id.twitch.tv/oauth2/token",
+	}
+
+	tokenSource := oauth2Conf.TokenSource(oauth2.NoContext)
+	reuseTokenSource := oauth2.ReuseTokenSource(token, tokenSource)
+
+	var err error;	
+	latestToken, err := reuseTokenSource.Token()
+	if err != nil {
+		log.Fatal("Got error when getting token from reuse source")
+	}
+
+	if os.Getenv("DEBUG") == "true" {
+		fmt.Printf("Token from reuse source: " + latestToken.AccessToken + "\n")
+	}
+
+	cacheTokenToDisk(latestToken)
+
+	return reuseTokenSource;
+}
